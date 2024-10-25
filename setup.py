76b837b82eb6aedfcb11b601c38249e09d4ab8c3
Felix Daniel Coca Calvimontes <daniel.uremix@gmail.com>
2024-10-24 23:53:13 -0400

    Constrictor name updated

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="constrictor",
+    name="constrictor-framework",
     version="1.0.4",
     packages=find_packages(),
     include_package_data=True,
