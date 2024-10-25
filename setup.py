33f2e92c049c4a7b63e05065b228890d3d689307
Felix Daniel Coca Calvimontes <daniel.uremix@gmail.com>
2024-10-24 23:53:28 -0400

    Constrictor version updated

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="constrictor-framework",
-    version="1.0.4",
+    version="1.0.0",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
